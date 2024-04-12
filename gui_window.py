0d853b5469ac9ef1784dd037c0ec5d4bd70304b4
Xin Lin <lxeptx4869@gmail.com>
2024-04-12 12:55:14 +0300

    modifying gui window class

--- gui_window.py
+++ gui_window.py
@@ -1,8 +1,65 @@
 from PyQt6 import QtWidgets, QtGui, QtCore
 
 from coordinates import Coordinates
+from robotworld import RobotWorld
+from robot import Robot
 
 class GuiWindow(QtWidgets.QMainWindow):
     def __init__(self, world, square_size):
         super().__init__()
+        self.added_robot = []
+        self.added_robot_gui = []
+        self.world = world
+        self.square_size = square_size
+
+        self.setCentralWidget(QtWidgets.QWidget())
+        self.horizontal = QtWidgets.QHBoxLayout()
+        self.centralWidget().setLayout(self.horizontal)
+
+
+    def init_window(self):
+        self.setGeometry(1300, 500, 800, 800)
+        self.setWindowTitle('Robot World')
+        self.show()
+
+        self.scene = QtWidgets.QGraphicsScene()
+        self.scene.setSceneRect(0, 0, 700, 700)
+
+        self.view = QtWidgets.QGraphicsView(self.scene, self)
+        self.view.adjustSize()
+        self.view.show()
+        self.horizontal.addWidget(self.view)
+
+    def create_world(self):
+        text,ok = QtWidgets.QInputDialog.getMultiLineText(self, 'Create Robot World', 'Enter world dimensions(width, height):')
+        if ok:
+            if text == "":
+                QtWidgets.QMessageBox.warning(self, "Error", "Please enter world dimensions")
+            elif len(text) != 2:
+                QtWidgets.QMessageBox.warning(self, "Error", "Please enter both dimensions in order: width, height!")
+            else:
+                world = RobotWorld(int(text.split(",")[0]), int(text.split(",")[1]))
+                QtWidgets.QMessageBox.information(self, "Success", "World created successfully!")
+
+
+    def create_robot(self):
+        text, ok = QtWidgets.QInputDialog.getText(self, 'Create Robot', 'Enter robot name:')
+        if ok:
+            Robot(text)
+            QtWidgets.QMessageBox.information(self, "Success", f"Robot {text} created successfully!")
+
+    def add_to_world(self):
+        text, ok = QtWidgets.QInputDialog.getText(self, 'Add Robot to World', 'Enter robot name:')
+        if ok:
+            
+
+
+
+
+
+    def init_button(self):
+        self.button_initbot = QtWidgets.QPushButton('Create Robot', self)
+        self.button_add.clicked.connect(self.create_robot)
+
         
+
